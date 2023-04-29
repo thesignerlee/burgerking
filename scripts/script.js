1ac3a2702363f4249dd2fb2561832d7b2ca09431
jongjealee <jongjealee@naver.com>
2023-04-29 14:47:46 +0900

    main

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,27 @@
-$('.swiper-slide').hide()
-$('.slide1').show()
+$('.bg_h').hide()
+$('.sub').hide()
+$('nav').on('mouseover',()=>{
+    $('.sub').stop(true).slideDown()
+    $('.bg_h').stop(true).slideDown()
+})
+$('nav').on('mouseout',()=>{
+    $('.sub').stop(true).slideUp()
+    $('.bg_h').stop(true).slideUp()
+})
+
+const swiper1 = new Swiper('.swiper',{
+    direction:'horizontal',
+    autoplay:{delay:2000},
+    loop:true,
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+})
+
+const a = document.querySelectorAll('.campaign3 p a')
+for(let i of a){
+    i.addEventListener('click',(e)=>{
+        e.preventDefault();
+    })
+}
